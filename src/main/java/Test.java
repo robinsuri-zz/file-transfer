6ef15dc9810f4ff4371fc802890f267cb25d98ac
robin <robinsuri@gmail.com>
2019-06-18 19:18:23 +0530

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,77 @@
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class Test {
+  private static Session session;
+
+  public static void main(String[] args) throws Exception {
+    LocalDateTime startLocal = LocalDateTime.now();
+    System.out.println("start = "+startLocal.toString());
+    long start = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+    uploadFiles(1000);
+    LocalDateTime endLocal = LocalDateTime.now();
+    System.out.println("end = "+endLocal.toString());
+    long end  = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+    System.out.println("diff = "+(end-start));
+    session.disconnect();
+
+  }
+
+  private static void uploadFiles(int times) throws Exception {
+    ChannelSftp channelSftp = getChannel();
+    for (int i = 1; i <= times; i++) {
+      InputStream inputStream = new FileInputStream("/Users/robin.su/Documents/Checkout/" +
+          "jsch-sftp-test-connect-1/src/main/java/upload.pdf");
+      Boolean isUploaded = uploadFile(inputStream, channelSftp);
+      inputStream.close();
+    }
+
+  }
+
+  private static ChannelSftp getChannel() {
+    ChannelSftp channelSftp = null;
+    try {
+      String userName = "cisco_sftp";
+      String password = "xwsBjXNo72V2UxI5kOg";
+      String host = "10.42.1.192";
+      JSch jSch = new JSch();
+      java.util.Properties config = new java.util.Properties();
+      config.put("StrictHostKeyChecking", "no");
+      session = jSch.getSession(userName, host);
+      session.setConfig(config);
+      session.setPassword(password);
+      session.connect();
+      channelSftp = (ChannelSftp) session.openChannel("sftp");
+      channelSftp.connect();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Inside channel");
+    }
+    return channelSftp;
+  }
+
+  private static Boolean uploadFile(InputStream inputStream, ChannelSftp channelSftp) {
+    try {
+      String fileName = "test" + LocalDateTime.now().toString();
+      channelSftp.put(inputStream, "/zetakycuat/" + fileName);
+    } catch (SftpException e) {
+      System.out.println(e);
+      return false;
+    }
+    return true;
+  }
+
+  private static void printThreadDetails() {
+    String message = String.format("isDaemon = %s isAlive = %s name = %s", Thread.currentThread().isDaemon(),
+        Thread.currentThread().isAlive(), Thread.currentThread().getName());
+    System.out.println(message);
+  }
+}
